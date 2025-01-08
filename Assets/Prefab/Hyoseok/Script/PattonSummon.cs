97d515d48fcf74b1492026899c2ff20c68b4b012
dksgytjr10 <dksgytjr10@gmail.com>
2025-01-08 17:53:48 +0900

    1. 뱀파이어 쉴드 , 이동, 소환 구현

--- Assets/Prefab/Hyoseok/Script/PattonSummon.cs
+++ Assets/Prefab/Hyoseok/Script/PattonSummon.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -6,7 +7,7 @@ namespace BS.vampire
 {
     public class PattonSummon : MonoBehaviour
     {
-        
+
         public GameObject batPrefab;
         public GameObject summonEffect;
         public GameObject shildEffect;
@@ -16,11 +17,16 @@ namespace BS.vampire
         public float summonInterval = 5f; // 소환 간격 
 
         private GameObject currentShild; //현재 쉴드 상태
+       public VamprieState vamprieState;
         void Start()
-        {       
+        {
+            if (vamprieState == null)
+            {
+                vamprieState = FindAnyObjectByType<VamprieState>();
+            }
             StartCoroutine(SummonBat());
         }
-      
+
         IEnumerator SummonBat()
         {
             while (true)
@@ -35,20 +41,22 @@ namespace BS.vampire
             bool Summoned = false;
             foreach (Transform location in summonLocations)
             {
-                // 현재 위치에 Bat 오브젝트가 없는지 확인
+                // 현재 위치에 Bat 확인
                 if (location.childCount == 0)
                 {
-                   GameObject bat = Instantiate(batPrefab, location.position, location.rotation, location);
+                    GameObject bat = Instantiate(batPrefab, location.position, location.rotation, location);
                     Summoned = true;
-           
+
                 }
             }
             if (Summoned)
             {
-                GameObject effectGo = Instantiate(summonEffect, effectLocation.position, effectLocation.rotation); 
+                GameObject effectGo = Instantiate(summonEffect, effectLocation.position, effectLocation.rotation);
+                effectGo.transform.parent = effectLocation.transform;
                 Destroy(effectGo, 3f);
             }
-            shild(Summoned);    
+            shild(Summoned);
+
         }
 
         //소환 몹 존재시 쉴드 생성 없을시 쉴드 파괴
@@ -56,25 +64,37 @@ namespace BS.vampire
         {
             if (batExist)
             {
-                if(currentShild == null)
-
+                if (currentShild == null)
                 {
-                  
                     currentShild = Instantiate(shildEffect, shildLocation.position, shildLocation.rotation);
+                    currentShild.transform.parent = shildLocation.transform;
+                    //보스 무적 온
+                    vamprieState.SetInvincible(true);   
                 }
             }
             else
             {
-                if (currentShild != null)
+                bool allEmpty = true;
+                foreach (Transform location in summonLocations)
+                {
+                    if (location.childCount > 0)
+                    {
+                        allEmpty = false; break;
+                    }
+                }
+                if (allEmpty && currentShild != null)
                 {
                     Destroy(currentShild);
                     currentShild = null;
+                    //보스 무적 해제
+                    vamprieState.SetInvincible(false);
+
                 }
             }
 
         }
 
-       
+
     }
 
 
---
+++ Assets/Prefab/Hyoseok/Script/VamprieTeleport.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using UnityEngine;
+
+namespace BS.vampire
+{
+    /// <summary>
+    /// 랜덤 이동 시스템 
+    /// 중앙이동시 CircleShoot발사
+    /// 외곽이동시 PingPongShot발사
+    /// </summary>
+    public class VamprieTeleport : MonoBehaviour
+    {
+        #region Variables
+        public ParticleSystem pingpongShot;
+        public GameObject CircleShot;
+        public Transform[] teleports; //순간이동 위치 0~3 랜덤 4는 중앙
+        public float time = 20; //순간이동 쿨타임
+        public Animator animator;
+        private int previousIndex = -1; //이전 위치값
+        #endregion
+
+        private void Start()
+        {
+          
+            //pingpongShot = pingpongShot.GetComponent<ParticleSystem>();
+            //var emission = pingpongShot.emission;
+            //emission.rateOverTime = 0f;
+            //CircleShot = CircleShot.GetComponent<ParticleSystem>();
+            if (animator == null)
+            {
+                animator = GetComponent<Animator>();
+            }
+            StartCoroutine(RandomTeleport());
+        }
+
+        IEnumerator RandomTeleport()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(time);
+                //애니메이션 연출 3초후에 이동
+                animator.SetTrigger("Teleport");
+                yield return new WaitForSeconds(2.5f);
+                int randomIndex;
+                do
+                {
+                    randomIndex = Random.Range(0, 4);
+                }
+                while(randomIndex ==previousIndex); //같은값 연속방지
+                //보스 위치를 랜덤이동
+                transform.position = teleports[randomIndex].position;
+                previousIndex = randomIndex;
+
+                if(randomIndex == 4)
+                {
+
+                    CircleShot.SetActive(true);
+                    yield return new WaitForSeconds(5f);
+                    CircleShot.SetActive(false);
+                }
+                else
+                {
+                    var emission = pingpongShot.emission;
+                    emission.rateOverTime = 25f;
+                    yield return new WaitForSeconds(2f);
+                    emission.rateOverTime = 0f;
+                   
+                }
+            }
+        }
+    }
+}
