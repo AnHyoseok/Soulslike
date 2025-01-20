f3869eded055dce8e883b891b23b15944127610b
c55702980 <c55702980@naver.com>
2025-01-20 18:03:35 +0900

    1. 오프닝 셋팅 중간 단계

--- Assets/Prefab/ChangYeol/Scripts/Demon/SceneManager.cs
+++ Assets/Prefab/ChangYeol/Scripts/Demon/SceneManager.cs
@@ -2,24 +2,29 @@ using BS.Player;
 using DG.Tweening;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 namespace BS.Demon
 {
     public class SceneManager : MonoBehaviour
     {
         #region Variables
-        public DemonController controller;
+        public DemonPattern pattern;
         public GameObject WarningCanvas;
         public Camera main;
         public GameObject drectingCamera;
         public GameObject bossCanvas;
-        public Animator animator;
         public GameObject presentEffect;
-        public GameObject player;
+        public PlayerController player;
+
+        private bool isPhase = false;
+        ToggleRendererFeature toggleRenderer;
         #endregion
 
         private void Start()
         {
+            toggleRenderer = GetComponent<ToggleRendererFeature>();
+            toggleRenderer.enabled = false;
             StartCoroutine(OpeningDemon());
         }
         void Update()
@@ -27,35 +32,65 @@ namespace BS.Demon
             WarningCanvas.transform.LookAt(WarningCanvas.transform.position + main.transform.rotation * Vector3.forward, main.transform.rotation * Vector3.up);
             if (Input.GetKeyDown(KeyCode.V))
             {
-                controller.TakeDamage(5);
+                pattern.demon.TakeDamage(5);
+            }
+            if (pattern.demon.hasRecovered && isPhase == false)
+            {
+                
+                StartCoroutine (PhaseDemon());
+                isPhase = true;
             }
         }
         IEnumerator OpeningDemon()
         {
-            
-            PlayerController playerController = player.GetComponent<PlayerController>();
-            controller.enabled = false;
-            playerController.enabled = false;
-            controller.gameObject.SetActive(false);
-            GameObject eff = Instantiate(presentEffect, controller.gameObject.transform.position, presentEffect.transform.rotation);
+            pattern.demon.enabled = false;
+            player.enabled = false;
+            pattern.demon.gameObject.SetActive(false);
+            GameObject eff = Instantiate(presentEffect, pattern.gameObject.transform.position, presentEffect.transform.rotation);
             yield return new WaitForSeconds(0.2f);
-            controller.gameObject.SetActive(true);
-            float descentDuration = 7f; // 내려오는 데 걸리는 시간
+            pattern.demon.gameObject.SetActive(true);
+            float descentDuration = 4f; //작아지는 시간
             float elapsedTime = 0f;
             
-            yield return new WaitForSeconds(1.2f);
+            yield return new WaitForSeconds(0.5f);
+            ToggleRendererFeature toggleRenderer = GetComponent<ToggleRendererFeature>();
+            toggleRenderer.enabled = true;
             while (elapsedTime < descentDuration)
             {
-                eff.transform.localScale = Vector3.Lerp(eff.transform.localScale, new Vector3(0.1f,0.1f,0.1f)*-0.5f, elapsedTime / descentDuration);
+                eff.transform.localScale = Vector3.Lerp(eff.transform.localScale, new Vector3(0.1f,0.1f,0.1f)*-0f, elapsedTime / descentDuration);
                 elapsedTime += Time.deltaTime;
+                // 목표 크기에 도달했는지 확인
+                if (Vector3.Distance(transform.localScale, new Vector3(0.1f, 0.1f, 0.1f) * -0f) < 0.01f)
+                {
+                    Destroy(eff,0.5f);
+                }
                 yield return null;
             }
-            if(eff.transform.localScale == Vector3.zero)
-            {
-                Destroy(eff,1);
-            }
-
             //TODO : 카메라 흔들면서 연출 효과 나오고 이름 나오고 시작
+
+            yield return new WaitForSeconds(0.5f);
+            toggleRenderer.enabled = false;
+            drectingCamera.SetActive(false);
+            pattern.demon.enabled = true;
+            pattern.demon.ChangeState(DEMON.Idle);
+            player.enabled = true;
+            yield return null;
+        }
+        IEnumerator PhaseDemon()
+        {
+            drectingCamera.SetActive(!isPhase);
+            Debug.Log("dd");
+            yield return new WaitForSeconds(0.5f);
+            pattern.demon.enabled = false;
+            player.enabled = false;
+
+            yield return new WaitForSeconds(10f);
+            //TODO : 카메라 흔들리면서 시작
+
+            drectingCamera.SetActive(false);
+            pattern.demon.enabled = true;
+            pattern.demon.ChangeState(DEMON.Idle);
+            player.enabled = true;
             yield return null;
         }
     }
