302403b305f138934f3a15f49aea859b4430bf6d
toneed <dydgns0729@gmail.com>
2025-01-20 20:22:57 +0900

    1. Set Opening 구현 (Sequence Camera + Animation + Particle이용)

---
+++ Assets/Prefab/Younghoon/Scripts/SetOpening.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Cinemachine;
+
+namespace BS.Enemy.Set
+{
+    public class SetOpening : MonoBehaviour
+    {
+        #region Variables
+        [SerializeField] GameObject player;
+        [SerializeField] Canvas playerCanvas;
+        [SerializeField] GameObject boss;
+        [SerializeField] ParticleSystem chargingParticle;
+        [SerializeField] ParticleSystem explosionParticle;
+        [SerializeField] CinemachineSequencerCamera cutSceneCamera;
+        Animator animator;
+
+        #endregion
+
+        private void Awake()
+        {
+            player.SetActive(false);
+            playerCanvas.gameObject.SetActive(false);
+            boss.SetActive(false);
+            chargingParticle.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+            explosionParticle.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+        }
+
+        void Start()
+        {
+            animator = GetComponent<Animator>();
+            StartCoroutine(Opening());
+        }
+
+
+        IEnumerator Opening()
+        {
+            yield return new WaitForSeconds(1f);
+            animator.SetTrigger(SetProperty.SET_ANIM_TRIGGER_ROAR);
+            yield return new WaitForSeconds(1f);
+            chargingParticle.Play();
+            yield return new WaitForSeconds(2.2f);
+            chargingParticle.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+            explosionParticle.Play();
+            yield return new WaitForSeconds(4.5f);
+
+            player.SetActive(true);
+            playerCanvas.gameObject.SetActive(true);
+            boss.SetActive(true);
+            cutSceneCamera.gameObject.SetActive(false);
+
+            Destroy(gameObject);
+        }
+
+        private void OnDestroy()
+        {
+            StopCoroutine(Opening());
+        }
+    }
+}
