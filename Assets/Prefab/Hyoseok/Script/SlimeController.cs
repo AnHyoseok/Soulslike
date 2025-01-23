737a389bf55de7644aa9747cacdcd0ca2b1c7179
dksgytjr10 <dksgytjr10@gmail.com>
2025-01-23 10:32:24 +0900

    1. 슬라임 컨트롤러 수정

--- Assets/Prefab/Hyoseok/Script/SlimeController.cs
+++ Assets/Prefab/Hyoseok/Script/SlimeController.cs
@@ -8,48 +8,57 @@ namespace BS.Title
     {
         #region Variables
         public GameObject player;
-        public GameObject attackPaticle;
+        public GameObject attackParticlePrefab; // 파티클 프리팹
+        public Transform attackParticleSpawnPoint; // 파티클 생성 위치
         public int damageAmount = 1;
-
-        //private Animator animator;
+        private Animator animator;
+        private bool isAttacking = false; // 공격 중인지 여부를 확인하는 플래그
         #endregion
 
         private void Start()
         {
-            //if (animator == null)
-            //{
-            //    animator = GetComponent<Animator>();
-            //}
-            //StartCoroutine(Attak());
-        }
-        private void Update()
-        {
-            transform.LookAt(player.transform);
-           
+            if (animator == null)
+            {
+                animator = GetComponent<Animator>();
+            }
+            StartCoroutine(AttackLoop()); // 공격 루프 시작
         }
 
         void OnTriggerEnter(Collider other)
         {
-
-            // 자식 객체에서 PlayerHealth 컴포넌트를 찾음
             PlayerHealth playerHealth = other.GetComponentInChildren<PlayerHealth>();
             if (playerHealth != null)
             {
                 Debug.Log($"{damageAmount}만큼 데미지 입음");
                 playerHealth.TakeDamage(damageAmount, false);
                 Debug.Log($"hp={playerHealth.CurrentHealth}");
-
             }
-
         }
-        //IEnumerator Attak()
-        //{
-        //    animator.SetTrigger("attack");
-        //    attackPaticle.SetActive(true);
-        //    yield return new WaitForSeconds(1f);
 
-        //    attackPaticle.SetActive(false);
-        //}
- 
+        IEnumerator AttackLoop()
+        {
+            while (true)
+            {
+                if (!isAttacking)
+                {
+                    yield return StartCoroutine(Attack());
+                }
+                yield return new WaitForSeconds(1f); // 1초마다 공격
+            }
+        }
+
+        IEnumerator Attack()
+        {
+            isAttacking = true; // 공격 시작
+            animator.SetBool("attack", true);
+
+            yield return new WaitForSeconds(0.7f);
+            GameObject attackParticle = Instantiate(attackParticlePrefab, attackParticleSpawnPoint.position, attackParticleSpawnPoint.rotation);
+            yield return new WaitForSeconds(1f);
+
+            Destroy(attackParticle,1f); // 파티클 제거
+            animator.SetBool("attack", false);
+            isAttacking = false; // 공격 종료
+        }
     }
 }
